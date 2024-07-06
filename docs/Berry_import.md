5d69f274c1264adad55905459dd5ab13ac1a56c7
Staars <baars@klinikum-brandenburg.de>
2024-07-06 18:38:23 +0200

    some tests

---
+++ docs/Berry_import.md
@@ -0,0 +1,115 @@
+# Berry Python Import Assistant
+
+<script src="../extra_javascript/python2berry.js"></script>
+
+<textarea name="python_src" id="python_src" rows="20" cols="50">Source
+</textarea>
+
+<br>
+<button onclick="python2berry()">Convert to Berry</button>
+<br>
+
+
+<textarea name="berry_src" id="berry_src" rows="20" cols="50">
+</textarea>
+
+
+
+### `img` class
+
+Thin wrapper for image data, that allows format conversions and is able to reduce memory reallocations in certain scenarios.  
+  
+Supports following image types, which integer values are equal to the enum `pixformat_t` of Espressif's webcam driver:  
+- `img.RGB565`      = 0  
+- `img.RGB888`      = 5  
+- `img.JPEG`        = 4  
+- `img.GRAYSCALE`   = 3  
+
+Create an instance of an image with `var i = img()`.  
+Memory will be released automatically by Berrys garbage collecter after deletion of the instance.  
+
+img Function|Parameters and details
+:---|:---
+from_jpeg|`img.from_jpeg(jpeg_buffer:bytes[, type:img.type]) -> nil` Copy JPEG image as byte buffer to the buffer of an `img` instance. If optional image type is provided, this will be converted on the fly. This will not reallocate the image buffer, if the size and format does not change.
+from_buffer|`img.from_buffer(image_data:bytes,width:int:height:int,type:img.type) -> nil` Construct image from raw image data for the types `RGB565`, `RGB888` and `GRAYSCALE`.
+get_buffer|`img.get_buffer([descriptor:bytes]) -> image_data:bytes` Returns the raw image data for any supported type. For `RGB565`, `RGB888` and `GRAYSCALE` a descriptor can be provided to get a ROI (region of interest).
+convert_to|`img.convert_to(type:img.type) -> nil` Internal conversion of the image format.
+info|`img.info() -> map` Returns a map with some infos about the current image.
+  
+The optional ROI descriptor is a representation of an affine matrix, which can be constructed in Berry:
+```berry
+def roi_dsc(m)
+    var d = bytes(-24)
+    d.setfloat(0,m["scaleX"])
+    d.setfloat(4,m["shearX"])
+    d.setfloat(8,m["shearY"])
+    d.setfloat(12,m["scaleY"])
+    d.seti(16,m["transX"],2)
+    d.seti(18,m["transY"],2)
+    d.seti(20,m["width"],2)
+    d.seti(22,m["height"],2)
+    return d
+end
+```
+  
+A simple web tool, to create such matrices is provided in the docs.
+  
+
+### `cam` module
+
+Very small module to access a connected camera module with the purpose to have as much heap memory available as possible in comparison to the fully fledged webcam drivers for machine learning, but there are more possible applications. It is not intended to be a general replacement for the webcam drivers.
+  
+Tasmota Function|Parameters and details
+:---|:---
+cam.setup|`(mode:int) -> bool` Init camera hardware with the resolution (same value as command `wcresolution`).
+cam.get_image|`([image:img[,type:img.type]]) -> bytes or nil` Takes a picture - without an additional option this is just a JPEG buffer. If an image instance is provided, the image data will go there. If an additional type is given, a conversion will happen on the fly. This will not lead to a memory reallocation, if there is no change for size and type of the image.
+cam.info|`() -> map` Shows info map with last current resolution and camera mode 
+
+Example:  
+
+``` berry
+
+# set background color to blue
+scr = lv.scr_act()
+scr.set_style_bg_color(lv.color(lv.COLOR_BLUE), lv.PART_MAIN | lv.STATE_DEFAULT)
+
+# create a lv_img object as image view
+cam_view = lv.img(scr)
+cam_view.center()
+
+i = img()
+
+import cam
+cam.setup(4) # 240 x 240
+cam.get_image(i,i.RGB565)
+
+def lv_img_dsc(image)
+    var i = image.info()
+    var dsc = bytes(24)
+    dsc..0x19 # magic
+    dsc..0x12 # cf RGB565
+    dsc.add(0,2) # flags
+    dsc.add(i["width"],2) # width
+    dsc.add(i["height"],2) # height
+    dsc.add(i["width"] * 2,2) # stride
+    dsc.add(0,2) # reserved
+    dsc.add(i["size"],4) # size
+    dsc.add(i["buf_addr"],4) # data
+    dsc.add(0,4) # reserved
+    print(dsc)
+    return dsc
+end
+
+descriptor = lv_img_dsc(i)
+cam_view.set_src(descriptor) # bind cam_view to buffer of img
+
+def video()
+    cam.get_image(i,i.RGB565) # this will just update the buffer, no reallocation
+    cam_view.invalidate()
+    tasmota.set_timer(20,/->video()) # aim for 50 Hz
+end
+
+video()
+```
+
+
---
+++ docs/extra_javascript/python2berrry.js
@@ -0,0 +1,172 @@
+class Transpiler {
+
+  code;
+  output;
+  preventTerminationWords = ["else","elif","except"];
+  needTerminationWords = ["if","class","def","while","for","try"];
+  opening =["(","{","["];
+  closing =[")","}","]"];
+  multiline;
+
+  constructor(code) {
+    this.code = code;
+  }
+
+  refactor_line(line, isOneliner){
+    var refactored = line.replace(/:+$/, ""); // strip :
+    if(isOneliner){
+      const _l = line.split(":");
+      _l.push("end");
+      refactored = _l.join(" ");
+    };
+    refactored = refactored.replace("None","nil");
+    refactored = refactored.replace("float","real");
+    refactored = refactored.replace("True","true");
+    refactored = refactored.replace("False","false");
+    refactored = refactored.replace(" and "," && ");
+    refactored = refactored.replace(" or "," || ");
+    refactored = refactored.replace(" not "," !");
+    refactored = refactored.replace("@staticmethod","static");
+    refactored = refactored.replace("__init__","init");
+    return refactored;
+  }
+
+  spaces(count){
+    var spaces = "";
+    for(let i=0;i < count; i++){
+      spaces += " ";
+    }
+    return spaces;
+  }
+
+  exitLine(line){
+    var exit = false;
+    if(line.trim().length == 0){ //empty line
+      this.output += line + "\n";
+      exit = true; //ignore
+    }
+    else if(line.trim().startsWith("#")){ //comment line
+      this.output += line + "\n";
+      exit = true; //ignore
+    }
+    return exit;
+  }
+
+  oneLiner(line){
+    var result = false;
+    var l = line.trim();
+    if(l.startsWith("if")){
+      var i = l.split(":");
+      if(i.length > 1){
+        console.log("Oneliner:",i);
+        result = true;
+      }
+    }
+    return result;
+  }
+
+  completeLine(line){
+    var open_brackets = 0;
+    this.multiline += line;
+    for (let i = 0; i < this.multiline.length; i++) {
+      let c = this.multiline[i];
+      if (this.opening.some(char=>c == char)){
+        open_brackets += 1;
+      };
+      if (this.closing.some(char=>c == char)){
+        open_brackets -= 1;
+      };
+    }
+    if(open_brackets > 0){
+      // console.log("Accumulating:",line);
+      this.output += this.refactor_line(line);
+      this.output += "\n";
+      return false;
+    }
+    else{
+      // console.log("Complete:",this.multiline);
+      this.multiline = "";
+      return true;
+    }
+  }
+
+  parse(){
+    var blocks_to_terminate = 0;
+    var indent_list = [0];
+    this.output = "";
+    this.multiline = "";
+
+    for(let line of this.code.split(/\r?\n/)){
+      var line_indent = line.search(/\S/);
+
+
+      if(this.completeLine(line) == false){ // or multiline
+        // console.log(line)
+        continue;
+      }
+
+      if(this.exitLine(line)){
+        continue;
+      }
+
+      var preventTermination = false;
+      if (this.preventTerminationWords.some(substring=>line.includes(substring))){
+        var o = this.output.split("\n");
+        console.log("remove last el",o);
+        o.splice(-1);
+        this.output = o.join("\n");
+        blocks_to_terminate += 1;
+        preventTermination = true;
+      }
+
+      var validKeyword = false;
+      if (this.needTerminationWords.some(substring=>line.trim().startsWith(substring))){
+          blocks_to_terminate += 1;
+          validKeyword = true;
+          console.log("blocks to terminate",blocks_to_terminate,"because of",line);
+          if(this.oneLiner(line)){
+            blocks_to_terminate -= 1;
+            // console.log("no, it is not!");
+          }
+      }
+
+      if(line_indent > indent_list[indent_list.length - 1]){
+        indent_list.push(line_indent);
+        console.log("New indentation in list",indent_list, line);
+      }
+
+      while((indent_list[indent_list.length - 1] > line_indent)){ //
+        indent_list.pop();
+        console.log("End block", indent_list, line_indent,"blocks_to_terminate",blocks_to_terminate);
+        if(preventTermination == false && blocks_to_terminate > 0){
+          this.output +=  this.spaces(indent_list[indent_list.length - 1]) + "end";
+          blocks_to_terminate -= 1;
+        }
+        this.output += "\n";
+      }
+      // console.log("list vs indent:",indent_list,line_indent)
+
+      this.output += this.refactor_line(line, this.oneLiner(line));
+      this.output += "\n";
+
+      this.indentation = line_indent;
+      // console.log("after ....", this.output,blocks_to_terminate);
+    }
+
+    console.log("Block:",this.output, blocks_to_terminate);
+    while(blocks_to_terminate > 0){
+      indent_list.pop();
+      this.output += this.spaces(indent_list[indent_list.length - 1]) + "end";
+      this.output += "\n";
+      blocks_to_terminate -= 1;
+    }
+    return this.output;
+  }
+};
+
+
+function python2berry() {
+  const python_src = document.getElementById("python_src").value;
+  let t = new Transpiler(python_src);
+  document.getElementById("berry_src").value = t.parse();
+}
